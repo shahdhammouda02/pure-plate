537141f59f5be909ad79e683a841190985ab0889
shahdhammouda02 <99508416+shahdhammouda02@users.noreply.github.com>
2025-11-10 13:16:52 +0300

    auth - session

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -20,11 +20,18 @@ const handler = NextAuth({
   pages: {
     signIn: "/auth/signin",
   },
-  callbacks: {
+   callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.includes("callbackUrl")) {
+        return url;
+      }
+      return `${baseUrl}/planner`;
+    },
     async session({ session }) {
       return session;
     },
   },
 });
+
 
 export { handler as GET, handler as POST };
